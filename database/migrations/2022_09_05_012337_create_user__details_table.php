23de9ef42e916bc9f6f220be82f60e42592d8087
Fahrikun <gunaon01@gmail.com>
2022-09-05 13:32:50 +0700

    database migrasi user_detail dan pngajuan datadiri

---
+++ database/migrations/2022_09_05_012337_create_user__details_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user__details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('name');
+            $table->string('npk')->unique();
+            $table->string('email')->unique();
+            $table->string('no_hp')->unique();
+            $table->string('tmp_lahir');
+            $table->date('tgl_lahir');
+            $table->date('tgl_bergabung');
+            $table->string('departemen');
+            $table->string('jabatan');
+            $table->string('gol');
+            $table->string('sta_kerja');
+            $table->string('sta_nikah');
+            $table->string('sta_pajak');
+            $table->string('no_bpjs_tk');
+            $table->string('no_bpjs_kes');
+            $table->string('no_dpa');
+            $table->string('no_npwp');
+            $table->integer('no_ktp');
+            $table->text('alamat_dom');
+            $table->text('alamat_ktp');
+            $table->string('nama_istri');
+            $table->integer('jml_anak');
+            $table->string('nama_anak1');
+            $table->string('nama_anak2');
+            $table->string('nama_anak3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user__details');
+    }
+}
---
+++ database/migrations/2022_09_05_025942_create_pengajuan__datadiris_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengajuanDatadirisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuan__datadiris', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('kode_p_datadiri')->unique();
+            $table->string('status');
+            $table->string('nama');
+            $table->string('tmp_lahir');
+            $table->date('tgl_lahir');
+            $table->string('sta_nikah');
+            $table->string('email')->unique();
+            $table->string('no_hp')->unique();
+            $table->string('no_npwp');
+            $table->integer('no_ktp');
+            $table->text('alamat_dom');
+            $table->text('alamat_ktp');
+            $table->string('nama_istri');
+            $table->integer('jml_anak');
+            $table->string('nama_anak1');
+            $table->string('nama_anak2');
+            $table->string('nama_anak3');
+            $table->string('file_bukti');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuan__datadiris');
+    }
+}
