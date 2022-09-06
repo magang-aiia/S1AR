4edf0995439ebe56b6814b3e6d6c212b0b1bb402
Kholan M <kholan.childs404@gmail.com>
2022-09-06 09:36:11 +0700

    Update filter tabel karyawan

--- resources/js/Components/ModalCuzia.vue
+++ resources/js/Components/ModalCuzia.vue
@@ -24,6 +24,8 @@
                 modalDetailIzin: false,
                 modalDetailDatadiri: false,
                 modalConfirm: false,
+                modalAddDetailDatadiri: false,
+                modalEditDetailDatadiri: false,
             }
         },
         methods: {
@@ -319,4 +321,194 @@
             </div>
         </Transition>
     </div>
+
+    <input type="checkbox" id="add-detail-datadiri" class="modal-toggle" v-model="modalAddDetailDatadiri" />
+    <label for="add-detail-datadiri" class="modal cursor-pointer bg-black/50 backdrop-blur transition-all ease-in-out">
+        <Transition name="bounce">
+            <label for="" v-if="modalAddDetailDatadiri" class="modal-box relative w-11/12 max-w-6xl">
+                <div class="mb-3 text-center text-3xl font-semibold">Tambah Data Karyawan</div>
+                <div class="mb-6 flex flex-wrap items-center -md:flex-col -md:justify-center">
+                    <div
+                        class="mr-3 w-36 overflow-hidden rounded-full border-4 border-gray-300 dark:border-gray-800 -md:mb-3 -md:w-48"
+                    >
+                        <img src="/img/avatar.jpg" />
+                    </div>
+                    <div class="max-w-[calc(100%_-_11rem)] -md:max-w-sm -md:text-center">
+                        <div class="max-w-full truncate text-2xl font-bold md:text-4xl">Budi Suherman</div>
+                        <div class="text-md max-w-full truncate opacity-60 md:text-lg">budi.suherman@aiia.co.id</div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-6 gap-x-6 gap-y-4">
+                    <div class="col-span-6 lg:col-span-2">
+                        <label for="name" class="mb-2 block">Nama Lengkap</label>
+                        <input type="text" name="name" id="name" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="email" class="mb-2 block">Email</label>
+                        <input type="email" name="email" id="email" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="no_hp" class="mb-2 block">No. Hp</label>
+                        <input type="text" name="no_hp" id="no_hp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="tmp_lahir" class="mb-2 block">Tempat Lahir</label>
+                        <input type="text" name="tmp_lahir" id="tmp_lahir" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="tgl_lahir" class="mb-2 block">Tanggal Lahir</label>
+                        <input type="text" name="tgl_lahir" id="tgl_lahir" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="sta_nikah" class="mb-2 block">Status Nikah</label>
+                        <input type="text" name="sta_nikah" id="sta_nikah" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="no_npwp" class="mb-2 block">No. NPWP</label>
+                        <input type="text" name="no_npwp" id="no_npwp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="no_ktp" class="mb-2 block">No. KTP</label>
+                        <input type="text" name="no_ktp" id="no_ktp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="alamat_dom" class="mb-2 block">Alamat Domisili</label>
+                        <input type="text" name="alamat_dom" id="alamat_dom" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="alamat_ktp" class="mb-2 block">Alamat KTP</label>
+                        <input type="text" name="alamat_ktp" id="alamat_ktp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_istri" class="mb-2 block">Nama Istri</label>
+                        <input type="text" name="nama_istri" id="nama_istri" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="jml_anak" class="mb-2 block">Jumlah Anak</label>
+                        <input type="text" name="jml_anak" id="jml_anak" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_anak_1" class="mb-2 block">Nama Anak Ke 1</label>
+                        <input type="text" name="nama_anak_1" id="nama_anak_1" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_anak_2" class="mb-2 block">Nama Anak Ke 2</label>
+                        <input type="text" name="nama_anak_2" id="nama_anak_2" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_anak_3" class="mb-3 block">Nama Anak Ke 3</label>
+                        <input type="text" name="nama_anak_3" id="nama_anak_3" class="input-text" />
+                    </div>
+                    <div class="col-span-6" v-if="isApprovable">
+                        <label for="nama" class="mb-2 block">Alasan Penolakan</label>
+                        <textarea
+                            rows="3"
+                            class="block w-full rounded-lg bg-base-100 disabled:cursor-not-allowed disabled:bg-base-300 dark:disabled:bg-black/70"
+                        ></textarea>
+                    </div>
+                    <div class="col-span-6 flex items-center justify-between md:flex-wrap-reverse">
+                        <label for="add-detail-datadiri" class="max-w-48 btn w-[calc(50%_-_.75rem)]">reset</label>
+                        <label for="add-detail-datadiri" class="max-w-48 btn btn-primary w-[calc(50%_-_.75rem)]"
+                            >Tambah</label
+                        >
+                    </div>
+                </div>
+            </label>
+        </Transition>
+    </label>
+
+    <input type="checkbox" id="edit-detail-datadiri" class="modal-toggle" v-model="modalEditDetailDatadiri" />
+    <label for="edit-detail-datadiri" class="modal cursor-pointer bg-black/50 backdrop-blur transition-all ease-in-out">
+        <Transition name="bounce">
+            <label for="" v-if="modalEditDetailDatadiri" class="modal-box relative w-11/12 max-w-6xl">
+                <div class="mb-3 text-center text-3xl font-semibold">Edit Data Karyawan</div>
+                <div class="mb-6 flex flex-wrap items-center -md:flex-col -md:justify-center">
+                    <div
+                        class="mr-3 w-36 overflow-hidden rounded-full border-4 border-gray-300 dark:border-gray-800 -md:mb-3 -md:w-48"
+                    >
+                        <img src="/img/avatar.jpg" />
+                    </div>
+                    <div class="max-w-[calc(100%_-_11rem)] -md:max-w-sm -md:text-center">
+                        <div class="max-w-full truncate text-2xl font-bold md:text-4xl">Budi Suherman</div>
+                        <div class="text-md max-w-full truncate opacity-60 md:text-lg">budi.suherman@aiia.co.id</div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-6 gap-x-6 gap-y-4">
+                    <div class="col-span-6 lg:col-span-2">
+                        <label for="name" class="mb-2 block">Nama Lengkap</label>
+                        <input type="text" name="name" id="name" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="email" class="mb-2 block">Email</label>
+                        <input type="email" name="email" id="email" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="no_hp" class="mb-2 block">No. Hp</label>
+                        <input type="text" name="no_hp" id="no_hp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="tmp_lahir" class="mb-2 block">Tempat Lahir</label>
+                        <input type="text" name="tmp_lahir" id="tmp_lahir" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="tgl_lahir" class="mb-2 block">Tanggal Lahir</label>
+                        <input type="text" name="tgl_lahir" id="tgl_lahir" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="sta_nikah" class="mb-2 block">Status Nikah</label>
+                        <input type="text" name="sta_nikah" id="sta_nikah" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="no_npwp" class="mb-2 block">No. NPWP</label>
+                        <input type="text" name="no_npwp" id="no_npwp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="no_ktp" class="mb-2 block">No. KTP</label>
+                        <input type="text" name="no_ktp" id="no_ktp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="alamat_dom" class="mb-2 block">Alamat Domisili</label>
+                        <input type="text" name="alamat_dom" id="alamat_dom" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="alamat_ktp" class="mb-2 block">Alamat KTP</label>
+                        <input type="text" name="alamat_ktp" id="alamat_ktp" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_istri" class="mb-2 block">Nama Istri</label>
+                        <input type="text" name="nama_istri" id="nama_istri" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="jml_anak" class="mb-2 block">Jumlah Anak</label>
+                        <input type="text" name="jml_anak" id="jml_anak" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_anak_1" class="mb-2 block">Nama Anak Ke 1</label>
+                        <input type="text" name="nama_anak_1" id="nama_anak_1" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_anak_2" class="mb-2 block">Nama Anak Ke 2</label>
+                        <input type="text" name="nama_anak_2" id="nama_anak_2" class="input-text" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label for="nama_anak_3" class="mb-3 block">Nama Anak Ke 3</label>
+                        <input type="text" name="nama_anak_3" id="nama_anak_3" class="input-text" />
+                    </div>
+                    <div class="col-span-6" v-if="isApprovable">
+                        <label for="nama" class="mb-2 block">Alasan Penolakan</label>
+                        <textarea
+                            rows="3"
+                            class="block w-full rounded-lg bg-base-100 disabled:cursor-not-allowed disabled:bg-base-300 dark:disabled:bg-black/70"
+                        ></textarea>
+                    </div>
+                    <div class="col-span-6 flex items-center justify-between md:flex-wrap-reverse">
+                        <label for="edit-detail-datadiri" class="max-w-48 btn w-[calc(50%_-_.75rem)]">reset</label>
+                        <label for="edit-detail-datadiri" class="max-w-48 btn btn-primary w-[calc(50%_-_.75rem)]"
+                            >Edit</label
+                        >
+                    </div>
+                </div>
+            </label>
+        </Transition>
+    </label>
 </template>
